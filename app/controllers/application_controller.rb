3098af371a758372221601f7276fe225243a8b94
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-13 18:54:09 -0800

    commit tree a0fe3782893e89aae645b706c33525fa89df0921

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+
+    # include associated reviews in the JSON response
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+
+    # include associated reviews in the JSON response
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all.order(price: :desc).limit(1)
+
+    # include associated reviews in the JSON response
+    baked_goods.to_json
+  end
 
 end
